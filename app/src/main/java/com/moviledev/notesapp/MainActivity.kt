f9a7612f74fcf807f4969503d3236e2e16c64e9a
ABIGAIL RODRÍGUEZ MOTA <ar2019-0095@uce.edu.do>
2021-11-16 22:13:36 -0400

    Initial commit

---
+++ app/src/main/java/com/moviledev/notesapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.moviledev.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
